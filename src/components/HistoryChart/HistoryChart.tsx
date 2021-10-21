63434d46d0f3590010a7570be306ad5703e72959
Abhishek Saxena <saxenaa218@gmail.com>
2021-10-21 21:15:54 +0530

    feat: added base file structure

---
+++ src/components/HistoryChart/HistoryChart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./HistoryChart.scss";
+
+const HistoryChart: React.FC<any> = (props) => {
+    return (
+        <section className="history-chart">
+            History Chart
+        </section>
+    )
+}
+
+export default HistoryChart;
