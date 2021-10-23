13e8a4ddc1865b0476963dd660f94ae5ba6c0a1d
Abhishek Saxena <saxenaa218@gmail.com>
2021-10-23 09:48:57 +0530

    refactor(Table and History): refactored UX

--- src/components/Table/components/TableRow/TableRow.tsx
+++ src/components/Table/components/TableRow/TableRow.tsx
@@ -7,8 +7,8 @@ const TableRow: React.FC<TableRowPropTypes> = ({ itm }) => {
     return (
         <tr className="table-row">
             <td>{itm.city}</td>
-            <td className={getAQIColorClass(itm.aqi)}>{itm.aqi}</td>
-            <td>{itm.timeStamp}</td>
+            <td className={getAQIColorClass(itm.aqi)}>{itm.aqi.toFixed(2)}</td>
+            <td>{new Date(itm.timeStamp).toLocaleString().replace(" GMT", "")}</td>
         </tr>
     )
 }
